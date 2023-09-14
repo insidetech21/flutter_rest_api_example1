4ce2d293d5e1ce1bb7e1868719cc22333f471adb
Vasant <vasant.basutkar@craveinfotech.com>
2023-09-14 12:58:08 +0530

    user model added, Home Page code updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'home.dart';
 
-void main() {
+void main()
+{
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
